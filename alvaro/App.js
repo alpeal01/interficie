e1a757d6a8add3f75a6acf9bea58c3588c199865
Alvaro Pérez Alborch <alpeal01@floridauniversitaria.es>
2022-12-15 17:38:35 +0100

    crack

--- alvaro/App.js
+++ alvaro/App.js
@@ -6,137 +6,67 @@
  * @flow strict-local
  */
 
- import React from 'react';
- import { Button } from 'react-native-paper';
- import { Switch } from 'react-native-paper';
- import { Chip } from 'react-native-paper';
- import { Surface, Text } from 'react-native-paper';
- import { Provider as PaperProvider } from 'react-native-paper';
- 
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   useColorScheme,
-   View,
- } from 'react-native';
- import { TextInput } from 'react-native-paper';
-  
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
-  
- /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
-  * LTI update could not be added via codemod */
- const Section = ({ children, title }) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
-  
- const saluda = (text) =>{ return( <Text style ={{color: 'blue',fontSize : 25} } >Hola {text}</Text>) }
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
 
- const saluda2 = () => {
-  return(
-    <View>
-  {saluda('caracola') }
-   {saluda('kurasan') }
-  </View>
-  )
- }
+const App = () => {
+    return (
+      
+      <View style={styles.contenidor}>
+          <View style={styles.seccio1}>
+              <Text>Secció 1</Text>
+          </View>
+          <View style={styles.seccio2}>
+            <Text>Secció 2</Text>
+          </View>
+          <View style={styles.seccio3}>
+            <Text>Secció 3</Text>
+          </View>
+      </View>
+    );
+};
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
-  
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
-   const isMarverl = true;
- 
-   return (
-
-    <PaperProvider>
-      {
-      isMarverl&&
-      saluda('Spiderman')
-      }
-      {saluda2()}
-
-</PaperProvider>
-   );
- };
-
-
-  
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
-   main: {
-    margin: 20,
-    fontSize : 20
-   },
-   buttAlien1:{
-    fontSize : 20,
-    marginTop : 20
-   },
-
-   container: {
+const styles = StyleSheet.create({
+  contenidor: {
     flex: 1,
-    marginTop: 100,
-    alignItems: "center"
+    flexDirection:'row',
+    borderColor:'purple',
+    borderWidth:5,
   },
-  row: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    paddingHorizontal: 12
+  seccio1: {
+    flex:1,
+    borderColor:'red',
+    borderWidth:3,
+    fontSize: 12,
+    fontWeight: '600',
+    margin: 4,
+    padding: 12,
+    textAlign: 'right',
   },
-  chip: {
-    margin: 4
+  seccio2: {
+    flex:1,
+    borderColor:'green',
+    borderWidth:3,
+    fontSize: 12,
+    fontWeight: '600',
+    margin: 4,
+    padding: 12,
+    textAlign: 'center',
+  },  
+  seccio3: {
+    flex:1,
+    borderColor:'blue',
+    borderWidth:3,
+    fontSize: 12,
+    fontWeight: '600',
+    margin: 4,
+    padding: 12,
+    textAlign: 'left',
   },
-  chipText: {
-    color: "#ffffff"
-  }
+});
 
- });
-  
- export default App;
- 
+export default App;
